36f5146580161ce0889c17641f3d333cd3d193fb
Oti254 <otrated555@gmail.com>
2026-01-25 23:45:09 +0300

    http URLs - prettifying the data

--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import (
 	"log"
 )
 
+const issueURL = "https://api.boot.dev/v1/courses_rest_api/learn-http/issues"
+
 func main() {
-	issues, err := getIssueData()
+	issues, err := getIssueData(issueURL)
 	if err != nil {
-		log.Fatalf("error getting issue data %v", err)
+		log.Fatalf("error getting issue data: %v", err)
 	}
-	// Converts a slice of bytes into a string
-	convertBytes := string(issues)
-	fmt.Println(convertBytes)
+	prettyData, err := prettify(string(issues))
+	if err != nil {
+		log.Fatalf("error prettifying data: %v", err)
+	}
+	fmt.Println(prettyData)
 }
