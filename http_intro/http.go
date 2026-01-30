f8c2b5d63f262de651d09c5c442097190058b4b3
Oti254 <otrated555@gmail.com>
2026-01-30 23:49:56 +0300

    update: adding uncommited intro to http files

--- http_intro/http.go
+++ http_intro/http.go
@@ -9,12 +9,15 @@ import (
 func getIssueData(url string) ([]byte, error) {
 	// http.Get uses the http.DefaultClient to make a request to the url
 	// res is the response from the server
+
 	res, err := http.Get(url)
 	if err != nil {
 		return nil, fmt.Errorf("error creating request: %w", err)
 	}
+	
 	// ensures response body is properly closed after reading
 	defer res.Body.Close()
+
 	// io.ReadAll reads the response body into a slice of bytes
 	data, err  := io.ReadAll(res.Body)
 	if err != nil {
