c78eecb20f86ff334f3c9121a9fbfeffa0c99ee4
Oti254 <otrated555@gmail.com>
2026-02-01 18:43:30 +0300

    web addresses

---
+++ dns/http_web_addresses.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"net/http"
+	"io"
+)
+
+func getIPAddress(domain string) (string, error) {
+	url := fmt.Sprintf("https://1.1.1.1/dns-query?name=%s&type=A", domain)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", fmt.Errorf("error creating request: %w", err)
+	}
+	req.Header.Set("accept", "application/dns-json")
+
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error sending request: %w", err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading response body: %w", err)
+	}
+	var dnsRes DNSResponse
+
+	err = json.Unmarshal(body, &dnsRes)
+	if err != nil {
+		return "", err
+	}
+
+	if len(dnsRes.Answer) == 0{
+		return "", fmt.Errorf("no answer found")
+	}
+
+	return dnsRes.Answer[0].Data, nil
+}
